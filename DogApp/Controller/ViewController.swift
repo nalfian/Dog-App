11ac932e05502f90b2192023f000ed7e21da58dd
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-08-23 06:54:16 +0700

    fetching mage

--- DogApp/Controller/ViewController.swift
+++ DogApp/Controller/ViewController.swift
@@ -25,7 +25,26 @@ class ViewController: UIViewController {
                 let decoder = JSONDecoder()
                 let imageData = try!
                     decoder.decode(DogImage.self, from: data)
-                print(imageData)
+                
+                guard let imageURL = URL(string: imageData.message) else{
+                    return
+                }
+                
+                let taskDownload = URLSession.shared.dataTask(
+                    with: imageURL,
+                    completionHandler: {(data,
+                                         response,
+                                         error) in
+                        guard let data = data else{
+                            return
+                        }
+                        let downloadImage = UIImage(data: data)
+                        DispatchQueue.main.async {
+                            self.imageView.image = downloadImage
+                        }
+                        
+                })
+                taskDownload.resume()
         }
         task.resume()
     }
