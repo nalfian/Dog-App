c6749f4614c173c52370988cc0cfdb6d8bdf7a30
Nur Alfian Julianda <nalfianjuli@gmail.com>
2019-08-22 10:59:17 +0700

    Initial Commit

---
+++ DogApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DogApp
+//
+//  Created by Unknown on 22/08/19.
+//  Copyright © 2019 Nalfian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
